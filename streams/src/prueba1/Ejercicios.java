1c2abeb11d5f88c68fedee81608957ce39a8c953
Angel G300 Saucedo <saucedog926@gmail.com>
2024-05-28 22:19:39 -0300

    ejercicio 2

--- streams/src/prueba1/Ejercicios.java
+++ streams/src/prueba1/Ejercicios.java
@@ -5,7 +5,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Ejercicio1 {
+public class Ejercicios {
+	/**
+	Ejercicio 1: Filtrar y Transformar una Lista de Números
+	Enunciado: 
+	Dada una lista de números enteros, filtra los números pares, eleva cada número par al cuadrado y recopila los resultados en una nueva lista.
+	 * */
 	public static void ejercicio1 () {
 		
 		List<Integer> listaNumeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11);
@@ -23,7 +28,6 @@ public class Ejercicio1 {
 	public static void ejercicio1_parallels () {
 		
 		List<Integer> listaNumeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11);
-		
 		Stream<Integer> listaStream = listaNumeros.parallelStream();
 		
 		List<Integer> resultado = listaStream.filter(n-> n % 2 == 0)
@@ -34,9 +38,26 @@ public class Ejercicio1 {
 		
 	}
 	
-	public static void main(String args[]) {
-		Ejercicio1.ejercicio1_parallels();
+	public static void ejercicio2() {
+		/**
+	Ejercicio 2: Filtrar y Contar Palabras
+	Enunciado: 
+	Dada una lista de palabras, filtra las palabras que tienen más de 3 caracteres y conviértelas a minúsculas.
+	Cuenta cuántas palabras cumplen esta condición.
+		* */
+		List<String> lista = Arrays.asList("asd","dsa","hola","chau","saludo","saludooo","asdasdasdas");
 		
+		
+		long listaResultado = lista.stream()
+									.filter(p -> p.length() > 3)
+									.map(r -> r.toLowerCase())
+									.count();
+		
+		System.out.println(listaResultado);
+	}
+	
+	public static void main(String args[]) {
+		Ejercicios.ejercicio2();
 	}
 	
 }
