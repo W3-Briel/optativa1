735fd0333580db9d889a0223a6f8387d1390b8fc
Angel G300 Saucedo <saucedog926@gmail.com>
2024-06-04 18:38:49 -0300

    activity 2-4

--- streams/src/actividad/Actividad.java
+++ streams/src/actividad/Actividad.java
@@ -1,42 +1,94 @@
 package actividad;
 
 import java.util.Arrays;
+import java.util.List;
+
 import java.util.stream.*;
 
+class NegativeRange extends RuntimeException{
+	public NegativeRange() {
+		super("no puedes ingresar un valor negativo como rango!");
+	}
+}
 
 public class Actividad {
-
-//	Enunciado 2: Desarrolla una aplicación en Java que procese una lista de strings y 
-//				busque las palabras que contienen una determinada subcadena utilizando parallel streams para mejorar la eficiencia del proceso.
-//
-//	Enunciado 3: Escribe un programa en Java que utilice parallel streams para
-//				calcular el producto de los elementos de una lista de números enteros generados aleatoriamente y mostrar el resultado por pantalla.
-//
-//	Enunciado 4: Crea una aplicación en Java que utilice parallel streams para encontrar los números primos
-//				en un rango dado y mostrarlos en orden ascendente, aprovechando la capacidad de procesamiento paralelo para mejorar el rendimiento del cálculo.
-
-	
 	public void enunciado1(){
 //		Enunciado 1: Implementa un programa en Java que utilice parallel streams
 //		para calcular la suma de los cuadrados de los números enteros del 1 al 100 de manera paralela.
 		
-		// n*n + n1*n1 ...
+		// n0*n0 + n1*n1 ...
 		
-		int[] num = new int[100];
+		int total = IntStream.rangeClosed(1, 100)
+							.parallel()
+							.map(n -> n*n)
+							.sum();
 		
-		for (int i = 1; i<=100 ; i++) {
-			num[i-1] = i*i;
-		}
+		System.out.println(total);
+	}
+	
+	public void enunciado2(String sub, List<String> listStrings) {
+//		Enunciado 2: Desarrolla una aplicación en Java que procese una lista de strings y 
+//		busque las palabras que contienen una determinada subcadena utilizando parallel streams para mejorar la eficiencia del proceso.
 		
+		List<String> result = listStrings.parallelStream()
+										.filter(word -> word.contains((CharSequence) sub))
+										.collect(Collectors.toList());
+	
+		System.out.println(result);
+	}
+	
+	public void enunciado3() {
+//		Enunciado 3: Escribe un programa en Java que utilice parallel streams para
+//		calcular el producto de los elementos de una lista de números enteros generados aleatoriamente y mostrar el resultado por pantalla.
 		
-		System.out.println(Arrays.stream(num)
-								.parallel()
-								.sum());
+		final int listLength = 50;
+		final int maxRandomValue = 1000;
 		
+		int total = IntStream.rangeClosed(1, listLength)
+							.parallel()
+							.map(i -> (int) (Math.random() * maxRandomValue) + 1)
+							.sum();
+		
+		System.out.println(total);
+	}
+	
+	public void enunciado4(int range) {
+//		Enunciado 4: Crea una aplicación en Java que utilice parallel streams para encontrar los números primos
+//		en un rango dado y mostrarlos en orden ascendente, aprovechando la capacidad de procesamiento paralelo para mejorar el rendimiento del cálculo.
+		
+		if (range < 0) throw new NegativeRange();
+		
+		final int numberRange = range;
+		List<Integer> counsins = Stream.iterate(1,n -> n +1 )
+										.parallel()
+										.limit(numberRange)
+										.filter(num -> {
+											
+											if (num <= 1) return false;
+											
+											for (int i = 2; i <= Math.sqrt(num); i++) {
+									            if (num % i == 0) return false;
+										 	}
+											
+											return true;
+										})
+										.sorted()
+										.collect(Collectors.toList());
+		System.out.println(counsins);
 		
 	}
 	
 	public static void main(String args[]) {
+		
 		new Actividad().enunciado1();
+		
+		new Actividad().enunciado2("angel" , // sub cadena de ejemplo
+									Arrays.asList("hola","chau","adasdasangelasdasd","asdfasdfmateoasdasd") // lista de ejemplo
+									);
+		
+		new Actividad().enunciado3();
+		
+		new Actividad().enunciado4(-1);
+		
 	}
 }
